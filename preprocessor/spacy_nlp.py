74ccc651c83b66b224b92f26b139db70e61226d8
Amartya Anayachala <amartya2704@gmail.com>
2025-06-19 02:36:05 +0530

    Debugs

--- preprocessor/spacy_nlp.py
+++ preprocessor/spacy_nlp.py
@@ -2,12 +2,13 @@ import streamlit as st
 import spacy
 from spacy.cli import download as spacy_download
 
-@st.cache_resource(ttl=3600)
 def load_spacy_nlp_model(model_name="en_core_web_sm"):
+    """
+    Loads a SpaCy model, downloading it if not already available.
+    """
     try:
         return spacy.load(model_name)
     except OSError:
-        # Try to download it if not present
         try:
             st.info(f"Downloading SpaCy model '{model_name}'...")
             spacy_download(model_name)
